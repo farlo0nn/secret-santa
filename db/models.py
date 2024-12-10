83e4a6d569a2f16d257a2d942aa7c334a25f1ed9
farlo0nn <farlo0nn@gmail.com>
2024-12-10 11:09:45 +0100

    edited db

--- db/models.py
+++ db/models.py
@@ -21,7 +21,7 @@ class User(Base):
     
     username = Column(String(255), unique=False, nullable=True)
 
-    created_rooms = relationship("Room", backref="admin")
+    created_rooms = relationship("Room", backref="admin", cascade="all,delete")
     rooms = relationship("Room", secondary=user_room, back_populates="users")
 
     give_to = relationship(
@@ -49,7 +49,7 @@ class Room(Base):
     admin_id = Column(Integer, ForeignKey("User.id"))
     users = relationship("User", secondary=user_room, back_populates="rooms")
     assignments = relationship("GiftAssignment", backref="room", cascade="all,delete")
-    wishes = relationship("Wish", backref="room")
+    wishes = relationship("Wish", backref="room", cascade="all,delete")
 
     def __repr__(self) -> str:
         return f"<Room:{self.code}>"
